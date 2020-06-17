d75dc655d9c76f3b76eb1733f52ce298b0b7ae15
LGouellec <sylvain.legouellec@hotmail.fr>
2020-06-17 23:35:59 +0200

    impl timewindows

--- core/Stream/TimeWindowOptions.cs
+++ core/Stream/TimeWindowOptions.cs
@@ -1,18 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Streamiz.Kafka.Net.Stream
 {
     public class TimeWindowOptions : WindowOptions<TimeWindow>
     {
-        public override long Size => throw new NotImplementedException();
+        private readonly long advanceMs;
 
-        public override long GracePeriodMs => throw new NotImplementedException();
+        public TimeWindowOptions(long sizeMs, long advanceMs)
+        {
+            Size = sizeMs;
+            this.advanceMs = advanceMs;
+            GracePeriodMs = sizeMs;
+        }
+
+        public override long Size { get; }
+
+        public override long GracePeriodMs { get; }
 
         public override IDictionary<long, TimeWindow> WindowsFor(long timestamp)
         {
-            throw new NotImplementedException();
+            long windowStart = (Math.Max(0, timestamp - Size + advanceMs) / advanceMs) * advanceMs;
+            IDictionary<long, TimeWindow> windows = new Dictionary<long, TimeWindow>();
+            while (windowStart <= timestamp)
+            {
+                TimeWindow window = new TimeWindow(windowStart, windowStart + Size);
+                windows.Add(windowStart, window);
+                windowStart += advanceMs;
+            }
+            return windows;
         }
+
+        public static TimeWindowOptions Of(long sizeMs)
+            => new TimeWindowOptions(sizeMs, sizeMs);
+
+        public static TimeWindowOptions Of(TimeSpan size)
+            => Of((long)size.TotalMilliseconds);
     }
 }
