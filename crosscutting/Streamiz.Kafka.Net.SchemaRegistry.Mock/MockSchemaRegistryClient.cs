d9122ddd2551dcffde3b44fd9f354d5e368e59e2
Sylvain Le Gouellec <sylvain.legouellec@hotmail.fr>
2021-02-23 17:16:05 +0100

    🔥 add documentation

--- crosscutting/Streamiz.Kafka.Net.SchemaRegistry.Mock/MockSchemaRegistryClient.cs
+++ crosscutting/Streamiz.Kafka.Net.SchemaRegistry.Mock/MockSchemaRegistryClient.cs
@@ -23,19 +23,56 @@ namespace Streamiz.Kafka.Net.SchemaRegistry.Mock
         private readonly Dictionary<string, List<RegisterSchema>> schemas = new Dictionary<string, List<RegisterSchema>>();
 
         #region ISchemaRegistryClient Impl
+        /// <summary>
+        /// The maximum capacity of the local schema cache. 
+        /// It's hardcoded to 100.
+        /// </summary>
         public int MaxCachedSchemas => 100;
 
+        /// <summary>
+        /// DEPRECATED. SubjectNameStrategy should now be specified via serializer configuration.
+        /// Returns the schema registry key subject name given a topic name.
+        /// </summary>
+        /// <param name="topic">The topic name.</param>
+        /// <param name="recordType">The fully qualified record type.</param>
+        /// <returns>The key subject name given a topic name.</returns>
+        [Obsolete("SubjectNameStrategy should now be specified via serializer configuration. This method will be removed in a future release.")]
         public string ConstructKeySubjectName(string topic, string recordType = null)
             => $"{topic}-key";
 
+        /// <summary>
+        /// DEPRECATED.SubjectNameStrategy should now be specified via serializer configuration.
+        /// Returns the schema registry value subject name given a topic name.
+        /// </summary>
+        /// <param name="topic">The topic name.</param>
+        /// <param name="recordType">The fully qualified record type.</param>
+        /// <returns>The value subject name given a topic name.</returns>
+        [Obsolete("SubjectNameStrategy should now be specified via serializer configuration. This method will be removed in a future release.")]
         public string ConstructValueSubjectName(string topic, string recordType = null)
             => $"{topic}-value";
 
-        public void Dispose() { }
+        /// <summary>
+        /// Disposable method.
+        /// </summary>
+        public void Dispose() 
+        {
+            subjects.Clear();
+            schemas.Clear();
+            id = 0;
+        }
 
+        /// <summary>
+        /// Gets a list of all subjects with registered schemas.
+        /// </summary>
+        /// <returns>A list of all subjects with registered schemas.</returns>
         public Task<List<string>> GetAllSubjectsAsync()
             => Task.FromResult(subjects);
 
+        /// <summary>
+        /// Get the latest schema registered against the specified subject.
+        /// </summary>
+        /// <param name="subject">The subject to get the latest associated schema for.</param>
+        /// <returns>The latest schema registered against subject.</returns>
         public Task<RegisteredSchema> GetLatestSchemaAsync(string subject)
         {
             if (schemas.ContainsKey(subject))
@@ -58,6 +95,12 @@ namespace Streamiz.Kafka.Net.SchemaRegistry.Mock
             }
         }
 
+        /// <summary>
+        /// Gets a schema given a subject and version number.
+        /// </summary>
+        /// <param name="subject">The subject to get the schema for.</param>
+        /// <param name="version">The version number of schema to get.</param>
+        /// <returns>The schema identified by the specified subject and version.</returns>
         public Task<RegisteredSchema> GetRegisteredSchemaAsync(string subject, int version)
         {
             if (schemas.ContainsKey(subject))
@@ -80,6 +123,12 @@ namespace Streamiz.Kafka.Net.SchemaRegistry.Mock
             }
         }
 
+        /// <summary>
+        /// Gets the schema uniquely identified by id.
+        /// </summary>
+        /// <param name="id">The unique id of schema to get.</param>
+        /// <param name="format">The format of the schema to get.</param>
+        /// <returns>The schema identified by id.</returns>
         public Task<Schema> GetSchemaAsync(int id, string format = null)
         {
             var schema = schemas
@@ -90,6 +139,14 @@ namespace Streamiz.Kafka.Net.SchemaRegistry.Mock
                 Task.FromResult((Schema)null);
         }
 
+        /// <summary>
+        /// DEPRECATED. Superseded by GetRegisteredSchemaAsync(string subject, int version)
+        /// Gets a schema given a subject and version number.
+        /// </summary>
+        /// <param name="subject">The subject to get the schema for.</param>
+        /// <param name="version">The version number of schema to get.</param>
+        /// <returns>The schema identified by the specified subject and version.</returns>
+        [Obsolete("Superseded by GetRegisteredSchemaAsync(string subject, int version). This method will be removed in a future release.")]
         public Task<string> GetSchemaAsync(string subject, int version)
         {
             if (schemas.ContainsKey(subject))
@@ -106,6 +163,13 @@ namespace Streamiz.Kafka.Net.SchemaRegistry.Mock
             }
         }
 
+        /// <summary>
+        /// Get the unique id of the specified avro schema registered against the specified subject.
+        /// </summary>
+        /// <param name="subject">The subject the schema is registered against.</param>
+        /// <param name="avroSchema">The schema to get the id for.</param>
+        /// <returns>The unique id identifying the schema.</returns>
+        [Obsolete("Superseded by GetSchemaIdAsync(string, Schema)")]
         public Task<int> GetSchemaIdAsync(string subject, string avroSchema)
         {
             if (schemas.ContainsKey(subject))
@@ -122,9 +186,20 @@ namespace Streamiz.Kafka.Net.SchemaRegistry.Mock
             }
         }
 
+        /// <summary>
+        /// Get the unique id of the specified schema registered against the specified subject.
+        /// </summary>
+        /// <param name="subject">The subject the schema is registered against.</param>
+        /// <param name="schema">The schema to get the id for.</param>
+        /// <returns>The unique id identifying the schema.</returns>
         public Task<int> GetSchemaIdAsync(string subject, Schema schema)
             => GetSchemaIdAsync(subject, schema.SchemaString);
 
+        /// <summary>
+        /// Gets a list of versions registered under the specified subject.
+        /// </summary>
+        /// <param name="subject">The subject to get versions registered under.</param>
+        /// <returns>A list of versions registered under the specified subject.</returns>
         public Task<List<int>> GetSubjectVersionsAsync(string subject)
         {
             if (schemas.ContainsKey(subject))
@@ -138,17 +213,46 @@ namespace Streamiz.Kafka.Net.SchemaRegistry.Mock
             }
         }
 
+        /// <summary>
+        /// Check if an avro schema is compatible with latest version registered against
+        /// a specified subject.
+        /// </summary>
+        /// <param name="subject">The subject to check.</param>
+        /// <param name="avroSchema">The schema to check.</param>
+        /// <returns>true if avroSchema is compatible with the latest version registered against a specified subject, false otherwise.</returns>
+        [Obsolete("Superseded by IsCompatibleAsync(string, Schema)")]
         public Task<bool> IsCompatibleAsync(string subject, string avroSchema)
             => Task.FromResult(true);
 
+        /// <summary>
+        /// Check if an avro schema is compatible with latest version registered against
+        /// a specified subject.
+        /// </summary>
+        /// <param name="subject">The subject to check.</param>
+        /// <param name="schema">The schema to check.</param>
+        /// <returns>true if avroSchema is compatible with the latest version registered against a specified subject, false otherwise.</returns>
         public Task<bool> IsCompatibleAsync(string subject, Schema schema)
             => IsCompatibleAsync(subject, schema.SchemaString);
 
+        /// <summary>
+        /// NotImplementedException
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="schema"></param>
+        /// <param name="ignoreDeletedSchemas"></param>
+        /// <returns></returns>
         public Task<RegisteredSchema> LookupSchemaAsync(string subject, Schema schema, bool ignoreDeletedSchemas)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Register an Avro schema or get the schema id if it's already registered.
+        /// </summary>
+        /// <param name="subject">The subject to register the schema against.</param>
+        /// <param name="avroSchema">The schema to register.</param>
+        /// <returns>A unique id identifying the schema.</returns>
+        [Obsolete("Superseded by RegisterSchemaAsync(string, Schema)")]
         public Task<int> RegisterSchemaAsync(string subject, string avroSchema)
         {
             ++id;
@@ -176,6 +280,12 @@ namespace Streamiz.Kafka.Net.SchemaRegistry.Mock
             return Task.FromResult(id);
         }
 
+        /// <summary>
+        /// Register an Avro schema or get the schema id if it's already registered.
+        /// </summary>
+        /// <param name="subject">The subject to register the schema against.</param>
+        /// <param name="schema">The schema to register.</param>
+        /// <returns>A unique id identifying the schema.</returns>
         public Task<int> RegisterSchemaAsync(string subject, Schema schema)
             => RegisterSchemaAsync(subject, schema.SchemaString);
 
