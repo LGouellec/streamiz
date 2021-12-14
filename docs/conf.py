b024fd134d69d6d17a53bb6861e1528344d4402c
LGouellec <sylvain.legouellec@hotmail.fr>
2021-12-14 15:09:41 +0100

    🎉 add github pages auto

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,10 @@ html_favicon = 'favicon.ico'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx_markdown_tables']
+extensions = [
+    'recommonmark',
+    'sphinx_markdown_tables',
+    'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
