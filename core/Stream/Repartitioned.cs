fef8caf9636862fef47451a43c20a26db8521c1d
LGouellec <sylvain.legouellec@hotmail.fr>
2022-02-14 11:59:57 +0100

    add xml doc + website doc

--- core/Stream/Repartitioned.cs
+++ core/Stream/Repartitioned.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO.MemoryMappedFiles;
 using Streamiz.Kafka.Net.SerDes;
 
 namespace Streamiz.Kafka.Net.Stream
@@ -11,11 +10,11 @@ namespace Streamiz.Kafka.Net.Stream
     /// <typeparam name="V">value type</typeparam>
     public class Repartitioned<K, V>
     {
-        public string Named { get; private set; }
-        public ISerDes<K> KeySerdes { get; private set; }
-        public ISerDes<V> ValueSerdes { get; private set; }
-        public int? NumberOfPartition { get; private set; }
-        public Func<string, K, V, int> StreamPartitioner { get; private set; }
+        internal string Named { get; private set; }
+        internal ISerDes<K> KeySerdes { get; private set; }
+        internal ISerDes<V> ValueSerdes { get; private set; }
+        internal int? NumberOfPartition { get; private set; }
+        internal Func<string, K, V, int> StreamPartitioner { get; private set; }
 
         private Repartitioned(
             string named,
@@ -34,44 +33,100 @@ namespace Streamiz.Kafka.Net.Stream
         #region Static
 
         internal static Repartitioned<K, V> Empty() => new(null, null, null, null, null);
+        
+        /// <summary>
+        /// Create a <see cref="Repartitioned{K,V}"/> instance with the provided name used as part of the repartition topic.
+        /// </summary>
+        /// <param name="name">name used as a processor name and part of the repartition topic name</param>
+        /// <returns>A new instance of <see cref="Repartitioned{K,V}"/></returns>
         public static Repartitioned<K, V> As(string name) => new(name, null, null, null, null);
+        
+        /// <summary>
+        /// Create a <see cref="Repartitioned{K,V}"/> instance with provided key and value serdes.
+        /// </summary>
+        /// <param name="keySerdes">Serdes to use the repartition key</param>
+        /// <param name="valueSerdes">Serdes to use the repartition value</param>
+        /// <returns>A new instance of <see cref="Repartitioned{K,V}"/></returns>
         public static Repartitioned<K, V> With(ISerDes<K> keySerdes, ISerDes<V> valueSerdes) => new(null, keySerdes, valueSerdes, null, null);
+        
+        /// <summary>
+        /// Create a <see cref="Repartitioned{K,V}"/> instance with provided key and value serdes.
+        /// </summary>
+        /// <typeparam name="KS">Repartition key serdes type</typeparam>
+        /// <typeparam name="VS">Repartition value serdes typer</typeparam>
+        /// <returns>A new instance of <see cref="Repartitioned{K,V}"/></returns>
         public static Repartitioned<K, V> Create<KS, VS>()
             where KS : ISerDes<K>, new()
             where VS : ISerDes<V>, new()
             => new(null, new KS(), new VS(), null, null);
 
+        /// <summary>
+        /// Create a <see cref="Repartitioned{K,V}"/> instance with provided number of partitions for repartition topic.
+        /// </summary>
+        /// <param name="numberPartitions">number of partitions used when creating repartition topic</param>
+        /// <returns>A new instance of <see cref="Repartitioned{K,V}"/></returns>
         public static Repartitioned<K, V> NumberOfPartitions(int numberPartitions) => new(null, null, null, numberPartitions, null);
+        
+        /// <summary>
+        /// Create a <see cref="Repartitioned{K,V}"/> instance with provided partitioner.
+        /// </summary>
+        /// <param name="streamPartitioner">the function used to determine how records are distributed among partitions of the topic</param>
+        /// <returns>A new instance of <see cref="Repartitioned{K,V}"/></returns>
         public static Repartitioned<K, V> Partitioner(Func<string, K, V, int> streamPartitioner) => new(null, null, null, null, streamPartitioner);
         
         #endregion
 
         #region Methods
         
+        /// <summary>
+        /// Set the repartition name (using for processor name and part of the repartition topic name)
+        /// </summary>
+        /// <param name="name">Repartition name</param>
+        /// <returns>this</returns>
         public Repartitioned<K, V> WithName(string name)
         {
             Named = name;
             return this;
         }
 
+        /// <summary>
+        /// Set the key serdes
+        /// </summary>
+        /// <param name="keySerdes">repartition key serdes</param>
+        /// <returns>this</returns>
         public Repartitioned<K, V> WithKeySerdes(ISerDes<K> keySerdes)
         {
             KeySerdes = keySerdes;
             return this;
         }
         
+        /// <summary>
+        /// Set the value serdes
+        /// </summary>
+        /// <param name="valueSerdes">repartition value serdes</param>
+        /// <returns>this</returns>
         public Repartitioned<K, V> WithValueSerdes(ISerDes<V> valueSerdes)
         {
             ValueSerdes = valueSerdes;
             return this;
         }
 
+        /// <summary>
+        /// Set the number of partitions for repartition topic
+        /// </summary>
+        /// <param name="numberOfPartitions">Number of partitions used during creation for repartition topic</param>
+        /// <returns>this</returns>
         public Repartitioned<K, V> WithNumberOfPartitions(int numberOfPartitions)
         {
             NumberOfPartition = numberOfPartitions;
             return this;
         }
 
+        /// <summary>
+        /// Set the stream partitioner
+        /// </summary>
+        /// <param name="streamPartitioner">Function to determine the partition where the repartition record will be persist</param>
+        /// <returns>this</returns>
         public Repartitioned<K, V> WithStreamPartitioner(Func<string, K, V, int> streamPartitioner)
         {
             StreamPartitioner = streamPartitioner;
