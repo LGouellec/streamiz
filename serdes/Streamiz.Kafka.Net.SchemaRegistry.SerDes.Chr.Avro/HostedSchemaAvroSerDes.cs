5de0008a626979ca4bb40853817c717777a3a22f
Alexandra Elena Prisecaru <alexandra_prisecaru@trimble.com>
2022-05-23 15:58:58 +0300

    Moved the HostedSchemaAvroSerDes to its own package

--- serdes/Streamiz.Kafka.Net.SchemaRegistry.SerDes.Chr.Avro/HostedSchemaAvroSerDes.cs
+++ serdes/Streamiz.Kafka.Net.SchemaRegistry.SerDes.Chr.Avro/HostedSchemaAvroSerDes.cs
@@ -5,7 +5,7 @@ using Streamiz.Kafka.Net.Errors;
 using Streamiz.Kafka.Net.SerDes;
 
 [assembly: InternalsVisibleTo("Streamiz.Kafka.Net.Tests,PublicKey=00240000048000009400000006020000002400005253413100040000010001000d9d4a8e90a3b987f68f047ec499e5a3405b46fcad30f52abadefca93b5ebce094d05976950b38cc7f0855f600047db0a351ede5e0b24b9d5f1de6c59ab55dee145da5d13bb86f7521b918c35c71ca5642fc46ba9b04d4900725a2d4813639ff47898e1b762ba4ccd5838e2dd1e1664bd72bf677d872c87749948b1174bd91ad")]
-namespace Streamiz.Kafka.Net.SchemaRegistry.SerDes.Avro
+namespace Streamiz.Kafka.Net.SchemaRegistry.SerDes.Chr.Avro
 {
     /// <summary>
     /// SerDes for hosted avro beans
