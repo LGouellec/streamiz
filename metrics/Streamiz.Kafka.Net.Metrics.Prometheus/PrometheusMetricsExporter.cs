b844f69e9e145297a8ddac9a93cd32bcf020b98a
LGouellec <sylvain.legouellec@hotmail.fr>
2022-12-07 14:05:08 +0100

    remove old code

--- metrics/Streamiz.Kafka.Net.Metrics.Prometheus/PrometheusMetricsExporter.cs
+++ metrics/Streamiz.Kafka.Net.Metrics.Prometheus/PrometheusMetricsExporter.cs
@@ -9,7 +9,6 @@ namespace Streamiz.Kafka.Net.Metrics.Prometheus
     public class PrometheusMetricsExporter
     {
         private readonly PrometheusRunner prometheusRunner;
-        private readonly IDictionary<string, Gauge> gauges = new Dictionary<string, Gauge>();
 
         public PrometheusMetricsExporter(PrometheusRunner prometheusRunner)
         {
@@ -19,21 +18,6 @@ namespace Streamiz.Kafka.Net.Metrics.Prometheus
         public void ExposeMetrics(IEnumerable<Sensor> sensors)
         {
             prometheusRunner.Expose(sensors);
-        }
-
-        private bool IsNumeric(object expression, out Double number)
-        {
-            if (expression == null)
-            {
-                number = Double.NaN;
-                return false;
-            }
-
-            return Double.TryParse( Convert.ToString( expression
-                    , CultureInfo.InvariantCulture)
-                , System.Globalization.NumberStyles.Any
-                , NumberFormatInfo.InvariantInfo
-                , out number);
         }
     }
 }
