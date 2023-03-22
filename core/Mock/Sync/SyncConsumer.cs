05e071a9a1de1d00fa1a6c68338dce2ff3688983
LGouellec <sylvain.legouellec@hotmail.fr>
2023-03-22 15:41:28 +0100

    try fixing unit test

--- core/Mock/Sync/SyncConsumer.cs
+++ core/Mock/Sync/SyncConsumer.cs
@@ -301,7 +301,7 @@ namespace Streamiz.Kafka.Net.Mock.Sync
             foreach (var kp in offsets)
             {
                 var clone = Assignment.ToList();
-                if (clone.Select(a => a.Topic).Contains(kp.Key))
+                if (clone != null && clone.Select(a => a.Topic).Contains(kp.Key))
                 {
                     if (timeout != TimeSpan.Zero && (dt + timeout) < DateTime.Now)
                         break;
