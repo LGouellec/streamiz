a86d2dd48282ff9fa286f13469fe0dead23ae885
LGouellec <sylvain.legouellec@hotmail.fr>
2023-06-07 14:33:00 +0200

    fix ut

--- test/Streamiz.Kafka.Net.Tests/Public/StreamConfigTests.cs
+++ test/Streamiz.Kafka.Net.Tests/Public/StreamConfigTests.cs
@@ -515,7 +515,7 @@ namespace Streamiz.Kafka.Net.Tests.Public
             Assert.AreEqual(40, globalConfig.MessageCopyMaxBytes);
             Assert.AreEqual(500, globalConfig.MessageMaxBytes);
             Assert.AreEqual(6, globalConfig.MetadataMaxAgeMs);
-            Assert.AreEqual(Confluent.Kafka.PartitionAssignmentStrategy.CooperativeSticky, globalConfig.PartitionAssignmentStrategy);
+            Assert.AreEqual(Confluent.Kafka.PartitionAssignmentStrategy.Range, globalConfig.PartitionAssignmentStrategy);
             Assert.AreEqual("D:", globalConfig.PluginLibraryPaths);
             Assert.AreEqual(800, globalConfig.QueuedMaxMessagesKbytes);
             Assert.AreEqual(1, globalConfig.QueuedMinMessages);
