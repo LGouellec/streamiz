3580eb1261b4d06ae78f1ab3a212e5a40ad46fa8
LGouellec <sylvain.legouellec@hotmail.fr>
2024-07-05 15:51:12 -0700

    #270 - WIP

---
+++ core/State/Internal/WindowStoreMaterializer.cs
@@ -0,0 +1,50 @@
+using System;
+using Streamiz.Kafka.Net.Crosscutting;
+using Streamiz.Kafka.Net.State.Supplier;
+using Streamiz.Kafka.Net.Stream;
+using Streamiz.Kafka.Net.Table;
+
+namespace Streamiz.Kafka.Net.State.Internal
+{
+    internal class WindowStoreMaterializer<K, V, W>
+        where W : Window
+    {
+        private readonly WindowOptions<W> windowsOptions;
+        private readonly Materialized<K, V, IWindowStore<Bytes, byte[]>> materializedInternal;
+
+        public WindowStoreMaterializer(WindowOptions<W> windowsOptions, Materialized<K, V, IWindowStore<Bytes, byte[]>> materializedInternal)
+        {
+            this.windowsOptions = windowsOptions;
+            this.materializedInternal = materializedInternal;
+        }
+
+        public IStoreBuilder<IWindowStore<K, V>> Materialize()
+        {
+            IWindowBytesStoreSupplier supplier = (IWindowBytesStoreSupplier)materializedInternal.StoreSupplier;
+            if (supplier == null)
+            {
+                if (windowsOptions.Size + windowsOptions.GracePeriodMs > materializedInternal.Retention.TotalMilliseconds)
+                    throw new ArgumentException($"The retention period of the window store { materializedInternal.StoreName } must be no smaller than its window size plus the grace period. Got size=[{windowsOptions.Size}], grace=[{windowsOptions.GracePeriodMs}], retention=[{materializedInternal.Retention.TotalMilliseconds}].");
+
+                supplier = Stores.DefaultWindowStore(
+                    materializedInternal.StoreName,
+                    materializedInternal.Retention,
+                    TimeSpan.FromMilliseconds(windowsOptions.Size));
+            }
+            else
+                supplier.WindowSize = !supplier.WindowSize.HasValue ? windowsOptions.Size : supplier.WindowSize.Value;
+
+            var builder = Stores.WindowStoreBuilder(supplier, materializedInternal.KeySerdes, materializedInternal.ValueSerdes);
+
+            if (materializedInternal.LoggingEnabled)
+                builder.WithLoggingEnabled(materializedInternal.TopicConfig);
+            else
+                builder.WithLoggingDisabled();
+
+            if (materializedInternal.CachingEnabled)
+                builder.WithCachingEnabled(materializedInternal.CacheSize);
+
+            return builder;
+        }
+    }
+}
