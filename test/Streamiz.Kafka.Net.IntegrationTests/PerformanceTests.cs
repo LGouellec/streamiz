ed15e2a8b47bf82c96436c2068e900ea2bc2510c
LGouellec <sylvain.legouellec@hotmail.fr>
2024-09-13 14:00:27 -0700

    Add a new integration test

--- test/Streamiz.Kafka.Net.IntegrationTests/PerformanceTests.cs
+++ test/Streamiz.Kafka.Net.IntegrationTests/PerformanceTests.cs
@@ -27,62 +27,17 @@ namespace Streamiz.Kafka.Net.IntegrationTests
         {
             kafkaFixture.DisposeAsync().Wait(TimeSpan.FromMinutes(5));
         }
-        
-        
-        [Test]
-        public async Task PerformanceTestStateful()
-        {
-            int numberResult = 100000;
-            var config = new StreamConfig<StringSerDes, StringSerDes>
-            {
-                ApplicationId = "performance-test", 
-                BootstrapServers = kafkaFixture.BootstrapServers,
-                AutoOffsetReset = AutoOffsetReset.Earliest,
-                LingerMs = 25
-            };
 
-            string inputTopic = "input-topic", outputTopic = "output-topic";
-        
-            await kafkaFixture.CreateTopic(inputTopic, 4);
-            await kafkaFixture.CreateTopic(outputTopic, 4);
-        
-            var builder = new StreamBuilder();
-            builder
-                .Stream<string, string>(inputTopic)
-                .GroupByKey()
-                .Count(RocksDb
-                        .As<String, Int64>("count-store")
-                        .WithKeySerdes(new StringSerDes())
-                        .WithValueSerdes(new Int64SerDes()))
-                .ToStream()
-                .To<StringSerDes, Int64SerDes>(outputTopic);
-        
-            var t = builder.Build();
-            var stream = new KafkaStream(t, config);
-        
-            kafkaFixture.ProduceRandomData(inputTopic, numberResult);
-            
-            await stream.StartAsync();
-            
-            var result = kafkaFixture.ConsumeUntil(outputTopic, numberResult, 60000);
-            
-            stream.Dispose();
-            
-            Assert.IsTrue(result);
-            
-            Directory.Delete(Path.Combine(config.StateDir, "performance-test"), true);
-        }
-        
-        [Test]
-        public async Task PerformanceRestoreTestStateful()
+        private async Task PerformanceRestore(ProcessingGuarantee guarantee, int numberOfRecords)
         {
-            int numberResult = 200000;
+            int numberResult = numberOfRecords;
             var config = new StreamConfig<StringSerDes, StringSerDes>
             {
                 ApplicationId = "performance-test-restore", 
                 BootstrapServers = kafkaFixture.BootstrapServers,
                 AutoOffsetReset = AutoOffsetReset.Earliest,
-                LingerMs = 25
+                Guarantee = guarantee,
+                LingerMs = 100
             };
 
             string inputTopic = "input-topic", outputTopic = "output-topic";
@@ -134,6 +89,66 @@ namespace Streamiz.Kafka.Net.IntegrationTests
             Directory.Delete(Path.Combine(config.StateDir, "performance-test-restore"), true);
             
             Console.WriteLine($"Restoration took approximately {endRestoration - startRestoration} ms");
+        }
+        
+        
+        [Test]
+        [NonParallelizable]
+        public async Task PerformanceTestStateful([Values(200000)] int x)
+        {
+            int numberResult = 100000;
+            var config = new StreamConfig<StringSerDes, StringSerDes>
+            {
+                ApplicationId = "performance-test", 
+                BootstrapServers = kafkaFixture.BootstrapServers,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                LingerMs = 25
+            };
+
+            string inputTopic = "input-topic", outputTopic = "output-topic";
+        
+            await kafkaFixture.CreateTopic(inputTopic, 4);
+            await kafkaFixture.CreateTopic(outputTopic, 4);
+        
+            var builder = new StreamBuilder();
+            builder
+                .Stream<string, string>(inputTopic)
+                .GroupByKey()
+                .Count(RocksDb
+                    .As<String, Int64>("count-store")
+                    .WithKeySerdes(new StringSerDes())
+                    .WithValueSerdes(new Int64SerDes()))
+                .ToStream()
+                .To<StringSerDes, Int64SerDes>(outputTopic);
+        
+            var t = builder.Build();
+            var stream = new KafkaStream(t, config);
+        
+            kafkaFixture.ProduceRandomData(inputTopic, numberResult);
+            
+            await stream.StartAsync();
+            
+            var result = kafkaFixture.ConsumeUntil(outputTopic, numberResult, 60000);
+            
+            stream.Dispose();
+            
+            Assert.IsTrue(result);
+            
+            Directory.Delete(Path.Combine(config.StateDir, "performance-test"), true);
+        }
+        
+        [Test]
+        [NonParallelizable]
+        public async Task PerformanceRestoreTestStateful([Values(200000)] int x)
+        {
+            await PerformanceRestore(ProcessingGuarantee.AT_LEAST_ONCE, x);
+        }
+
+        [Test]
+        [NonParallelizable]
+        public async Task PerformanceRestoreEOSTestStateful([Values(200000)] int x)
+        {
+            await PerformanceRestore(ProcessingGuarantee.EXACTLY_ONCE, x);
         }
     }
 }
